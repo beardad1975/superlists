64cf14aef82da4307a8fc7ce13c721016a91f0db
beardad1975 <beardad1975@gmail.com>
2015-09-29 01:23:52 +0800

    FT refactoring

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(  row_text , [row.text for row in rows])        
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -40,15 +45,9 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: 買孔雀羽毛')
 
-        import time
-        #time.sleep(10)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text ==  '1: 買孔雀羽毛' for row in rows),
-            "新的待辦事項沒有出現在表上 -- row.text是 :\n%s" % table.text,
-        )
+
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
@@ -58,13 +57,9 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
              
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn( '1: 買孔雀羽毛' , [row.text for row in rows])
-        self.assertIn(
-            '2: 用孔雀羽毛來飛',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('1: 買孔雀羽毛')
+        self.check_for_row_in_list_table('2: 用孔雀羽毛來飛')
+
         
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
