48bbffc2eb4b41a877221c6ec4374db8789d94f7
beardad1975 <beardad1975@gmail.com>
2015-09-30 01:02:57 +0800

    Redirect after POST , and show all the items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,10 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-from lists.views import home_page
 from django.template.loader import render_to_string
+
+from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -23,9 +25,52 @@ class HomePageTest(TestCase):
 
         response = home_page(request)
 
-        self.assertIn('一個新的待辦事項', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text' : '一個新的待辦事項'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, '一個新的待辦事項')
+
+
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = '一個新的待辦事項'
+
+        response = home_page(request)
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+        
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(),0)
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='待辦事項1')
+        Item.objects.create(text='待辦事項2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('待辦事項1', response.content.decode())
+        self.assertIn('待辦事項2', response.content.decode())
+        
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = '最開始的待辦事項'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = '第二個待辦事項'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual( first_saved_item.text, '最開始的待辦事項')
+        self.assertEqual( second_saved_item.text,'第二個待辦事項')
